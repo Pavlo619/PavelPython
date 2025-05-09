d8e585bb36fb9356ff1e36eca6df3bcbe6f05330
Pavlo619 <diakonov.pavel2011@gmail.com>
2025-05-09 12:22:20 +0300

    2

--- 08_lesson/project_test.py
+++ 08_lesson/project_test.py
@@ -1,70 +1,74 @@
 import requests
 
 
-base_url="https://ru.yougile.com/api-v2/projects"
-
+base_url = "https://ru.yougile.com/api-v2/projects"
 
 headers = {'authorization': 'Bearer '}
 
 
 def test_get_project():
-    resp=requests.get(base_url, headers=headers )
+    resp = requests.get(base_url, headers=headers)
     assert resp.status_code == 200
 
 
 def test_create_project():
     body = {
-    "title": "Домашняя работа 8",
-    "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
+        "title": "Домашняя работа 8",
+        "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post(base_url, headers=headers, json=body)
+    resp = requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
+
 
 def test_edit_project():
     body = {
-    "title": "Домашняя работа 8",
-    "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
+        "title": "Домашняя работа 8",
+        "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post(base_url, headers=headers, json=body)
+    resp = requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
-    id=resp.json()["id"]
+    id = resp.json()["id"]
     body = {
         "title": "Домашняя работа 8 отредактированный",
         "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp2=requests.put(base_url+id, headers=headers, json=body)
+    resp2 = requests.put(base_url+id, headers=headers, json=body)
     assert resp2.status_code == 200
+
 
 def test_get_one_project():
     body = {
-    "title": "Домашняя работа 8",
-    "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
+        "title": "Домашняя работа 8",
+        "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post(base_url, headers=headers, json=body)
+    resp = requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
-    id=resp.json()["id"]
-    resp2=requests.get(base_url+id, headers=headers)
+    id = resp.json()["id"]
+    resp2 = requests.get(base_url+id, headers=headers)
     assert resp2.status_code == 200
 
+
 def test_get_negative_project():
-    resp=requests.get(base_url)
+    resp = requests.get(base_url)
     assert resp.status_code == 401
 
+
 def test_create_negative_project():
-    resp=requests.post(base_url, headers=headers)
+    resp = requests.post(base_url, headers=headers)
     assert resp.status_code == 400
+
 
 def test_edit_negative_project():
     body = {
-    "title": "Домашняя работа 8",
-    "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
+        "title": "Домашняя работа 8",
+        "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post(base_url, headers=headers, json=body)
+    resp = requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
-    id=resp.json()["id"]
+    id = resp.json()["id"]
     body = {
         "title": "",
         "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp2=requests.put(base_url+id, headers=headers, json=body)
+    resp2 = requests.put(base_url+id, headers=headers, json=body)
     assert resp2.status_code == 400
