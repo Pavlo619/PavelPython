18c7c90c1748b84be6128c1795c5b27a834759d7
Pavlo619 <diakonov.pavel2011@gmail.com>
2025-05-09 12:06:01 +0300

    2

--- 08_lesson/project_test.py
+++ 08_lesson/project_test.py
@@ -1,11 +1,14 @@
 import requests
 
 
-headers = {'authorization': 'Bearer '}
+base_url="https://ru.yougile.com/api-v2/projects"
+
+
+headers = {'authorization': 'Bearer ofJjYruw4VTH6Y8hyeFAzbT6TrzgWNnanlS61SR4fgFT17yGKBRBTin9Tn71iEJr'}
 
 
 def test_get_project():
-    resp=requests.get("https://ru.yougile.com/api-v2/projects", headers=headers )
+    resp=requests.get(base_url, headers=headers )
     assert resp.status_code == 200
 
 
@@ -14,7 +17,7 @@ def test_create_project():
     "title": "Домашняя работа 8",
     "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post("https://ru.yougile.com/api-v2/projects", headers=headers, json=body)
+    resp=requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
 
 def test_edit_project():
@@ -22,14 +25,14 @@ def test_edit_project():
     "title": "Домашняя работа 8",
     "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post("https://ru.yougile.com/api-v2/projects", headers=headers, json=body)
+    resp=requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
     id=resp.json()["id"]
     body = {
         "title": "Домашняя работа 8 отредактированный",
         "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp2=requests.put("https://ru.yougile.com/api-v2/projects/"+id, headers=headers, json=body)
+    resp2=requests.put(base_url+id, headers=headers, json=body)
     assert resp2.status_code == 200
 
 def test_get_one_project():
@@ -37,18 +40,18 @@ def test_get_one_project():
     "title": "Домашняя работа 8",
     "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post("https://ru.yougile.com/api-v2/projects", headers=headers, json=body)
+    resp=requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
     id=resp.json()["id"]
-    resp2=requests.get("https://ru.yougile.com/api-v2/projects/"+id, headers=headers)
+    resp2=requests.get(base_url+id, headers=headers)
     assert resp2.status_code == 200
 
 def test_get_negative_project():
-    resp=requests.get("https://ru.yougile.com/api-v2/projects")
+    resp=requests.get(base_url)
     assert resp.status_code == 401
 
 def test_create_negative_project():
-    resp=requests.post("https://ru.yougile.com/api-v2/projects", headers=headers)
+    resp=requests.post(base_url, headers=headers)
     assert resp.status_code == 400
 
 def test_edit_negative_project():
@@ -56,12 +59,12 @@ def test_edit_negative_project():
     "title": "Домашняя работа 8",
     "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp=requests.post("https://ru.yougile.com/api-v2/projects", headers=headers, json=body)
+    resp=requests.post(base_url, headers=headers, json=body)
     assert resp.status_code == 201
     id=resp.json()["id"]
     body = {
         "title": "",
         "users": {"3585e821-a3b1-4f2e-9a03-ddfe153a77f7": "worker"}
     }
-    resp2=requests.put("https://ru.yougile.com/api-v2/projects/"+id, headers=headers, json=body)
+    resp2=requests.put(base_url+id, headers=headers, json=body)
     assert resp2.status_code == 400
