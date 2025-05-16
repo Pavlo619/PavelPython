1df3e338f38209c9318651826ea686dc277ba48d
Pavlo619 <diakonov.pavel2011@gmail.com>
2025-05-16 15:13:57 +0300

    2

---
+++ lesson_10/07_lesson/Calculator.py
@@ -0,0 +1,36 @@
+import allure
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from Calcelatorpage import CalculatorPage
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+@allure.severity(allure.severity.NORMAL)
+@allure.description("Тестирование калькулятора, проверка работы приложения")
+@allure.title("Тестирование сложения")
+def test_slow_calculator(driver):
+    page = CalculatorPage(driver)
+    page.open()
+    page.set_delay("45")
+
+    with allure.step("Нажатие на кнопки"):
+        for button in ['7', '+', '8', '=']:
+            page.click_button(button)
+
+    try:
+        # Увеличьте время ожидания, чтобы тест мог дождаться появления результата
+        result_text = WebDriverWait(driver, 60).until(
+            EC.text_to_be_present_in_element((By.ID, "result"), "15")
+        )
+        with allure.step("Получение ожидаемого результата"):
+            assert result_text == "15", f"Ожидался результат 15, но получен: {result_text}"
+    except Exception as e:
+        print(f"Ошибка: {e}")
---
+++ lesson_10/07_lesson/Pokupka.py
@@ -0,0 +1,29 @@
+import allure
+import pytest
+from selenium import webdriver
+from Pokupkapage import ShoppingCartPage
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+@allure.severity(allure.severity.NORMAL)
+@allure.description("Проверка формы покупки, для авторизованного пользователя")
+@allure.title("Тестирование формы покупки")
+def test_shopping_cart(driver):
+    page = ShoppingCartPage(driver)
+    page.open()
+    page.login("standard_user", "secret_sauce")
+
+    with allure.step("Проверка id товара"):
+        for product_id in ["sauce-labs-backpack", "sauce-labs-bolt-t-shirt", "sauce-labs-onesie"]:
+            page.add_product_to_cart(product_id)
+
+    page.go_to_cart()
+    page.proceed_to_checkout()
+    page.fill_checkout_form("Павел", "Дьяконов", "420087")
+
+    with allure.step("Проверка итоговой цены"):
+        assert page.get_total_price() == "Total: $58.29"
